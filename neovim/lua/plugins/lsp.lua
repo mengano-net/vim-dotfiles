fd784ff593f207b7baad014f8fd95c7769b3fe3c
mengano-net <28873858+mengano-net@users.noreply.github.com>
2021-08-25 17:59:48 +0000

    - got lua LSP working, followed this guide here: https://www.chrisatmachine.com/Neovim/28-neovim-lua-development/
    Signed-off-by: mengano-net <28873858+mengano-net@users.noreply.github.com>

--- neovim/lua/plugins/lsp.lua
+++ neovim/lua/plugins/lsp.lua
@@ -2,10 +2,10 @@
 
 vim.opt.completeopt = {'menuone', 'noinsert', 'noselect'}
 local nvim_lsp = require'lspconfig'
-local protocol = require'vim.lsp.protocol'
-local opts = { noremap=true, silent=true }
+-- local protocol = require'vim.lsp.protocol'
+-- local opts = { noremap=true, silent=true }
 
-
+--[[
 local on_attach = function(client, bufnr)
     require'completion'.on_attach()
 
@@ -19,13 +19,12 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', 'gD', ':lua vim.lsp.buf.definition()<CR>', opts)
     buf_set_keymap('n', 'gd', ':lua vim.lsp.buf.declaration()<CR>', opts)
 end
-
+]]
 
 nvim_lsp.pyright.setup {
     cmd = { '/usr/bin/pyright', '--stdio' },
     filetypes = { 'python' }
 }
-
 
 nvim_lsp.bashls.setup {
     filetypes = { 'sh', 'zsh' }
@@ -34,11 +33,51 @@ nvim_lsp.bashls.setup {
 nvim_lsp.vimls.setup {}
 
 -- turning off until IO figure out how to disable all those error that pop up on yaml
--- nvim_lsp.yamlls.setup{}
+nvim_lsp.yamlls.setup{}
+
+
+-- lua-language-server
+-- https://github.com/sumneko/lua-language-server/wiki/Build-and-Run-(Standalone)
+USER = vim.fn.expand('$USER')
+
+local sumneko_root_path = ""
+local sumneko_binary = ""
+
+if vim.fn.has("mac") == 1 then
+    sumneko_root_path = "/Users/" .. USER .. "/.local/bin/lua-language-server"
+    sumneko_binary = "/Users/" .. USER .. "/.config/nvim/lua-language-server/bin/macOS/lua-language-server"
+elseif vim.fn.has("unix") == 1 then
+    sumneko_root_path = "/home/" .. USER .. "/.local/lua-language-server"
+    sumneko_binary = "/home/" .. USER .. "/.local/lua-language-server/bin/Linux/lua-language-server"
+else
+    print("Unsupported system for sumneko")
+end
+
+require'lspconfig'.sumneko_lua.setup {
+    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = vim.split(package.path, ';')
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'}
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true, [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true}
+            }
+        }
+    }
+}
 
 
 -- local servers = {'pyright', 'yamlls', 'vimls'}
-local servers = { 'pyright', 'vimls', 'bashls' }
+local servers = { 'pyright', 'vimls', 'bashls', 'yamlls' }
     for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
     on_attach = on_attach,
