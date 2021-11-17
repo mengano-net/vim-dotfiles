b291dbe4bb26b6576a0a19335da2c1374e4cbc3e
mengano-net <28873858+mengano-net@users.noreply.github.com>
2021-11-17 02:42:37 +0000

    - formatting on the lualine section - removed old buffer plugin, replaced with bufferline.nvim
    Signed-off-by: mengano-net <28873858+mengano-net@users.noreply.github.com>

--- neovim/init.lua
+++ neovim/init.lua
@@ -6,44 +6,45 @@ require'plugins.telescope'                              -- my telescope options
 require'plugins.lexima'                                 -- my lexima options
 require'plugins.treesitter'                             -- my treesitter and LSP options
 require'plugins.lsp'                                    -- my treesitter and LSP options
+require'plugins.bufferline'
 local utils = require'utils'                            -- my own utlities module
 
 -- lualine plugin
 -- require('lualine').setup()
 require('lualine').setup {
-  options = {
-    theme = 'onedark',
-    -- theme = 'gruvbox',
-    icons_enabled = true,
-    extensions = {'fugitive'},
-  },
-  sections = {
-      lualine_a = {
-          'mode',
-          -- 'buffers'
-      },
-      lualine_b = {
-          'branch',
-          'diff',
-      },
-      lualine_c = {
-          {
-              'filename',           -- displays file status (readonly status, modified status)
-              file_status = true,   -- 0 = just filename, 1 = relative path, 2 = absolute path
-              path = 1,             -- Shortens path to leave 40 space in the window
-        }
-      },
-      lualine_x = {
-          {
-              'diagnostics',
-              sources = {"nvim_lsp"},
-              symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '},
-          },
-          'fileformat',
-          'filetype',
-      },
-      lualine_y = {},
-  }
+    options = {
+        theme = 'onedark',
+        -- theme = 'gruvbox',
+        icons_enabled = true,
+        extensions = {'fugitive'},
+    },
+    sections = {
+        lualine_a = {
+            'mode',
+            -- 'buffers',
+        },
+        lualine_b = {
+            'branch',
+            'diff',
+        },
+        lualine_c = {
+            {
+                'filename',           -- displays file status (readonly status, modified status)
+                file_status = true,   -- 0 = just filename, 1 = relative path, 2 = absolute path
+                path = 1,             -- Shortens path to leave 40 space in the window
+            }
+        },
+        lualine_x = {
+            {
+                'diagnostics',
+                sources = {"nvim_lsp"},
+                symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '},
+            },
+            'fileformat',
+            'filetype',
+        },
+        lualine_y = {},
+    }
 }
 
 -- devicons
@@ -200,7 +201,7 @@ require "paq" {
     'navarasu/onedark.nvim';
     'lifepillar/vim-gruvbox8';
 
-    'ap/vim-buftabline';                        -- show buffers on tabline
+    'akinsho/bufferline.nvim';                  -- show buffers on tabline
     'tpope/vim-fugitive';                       -- A Git wrapper
     'cohama/lexima.vim';                        -- Automated closing of parenthesis, etc
     'haya14busa/is.vim';                        -- Clear highlights when cursor moves
